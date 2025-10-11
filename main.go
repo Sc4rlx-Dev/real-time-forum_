024c647d1c9b4f816e9f4923eab222a89ad4cf43
oerraoui <itsscarl2022@gmail.com>
2025-10-11 02:02:26 +0100

    add main server setup

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+	dbinit "realtimeforum/internal/database/db-init"
+	"realtimeforum/internal/models"
+	"realtimeforum/internal/routes"
+	"realtimeforum/internal/utils"
+)
+var l = utils.NewRateLimiter()
+
+func init() {
+	var err error
+	models.Db, err = dbinit.OpenDatabase()
+	if err != nil { log.Fatal("Failed to open database:", err)
+		return
+	}
+	err = dbinit.Createtablue(models.Db)
+	if err != nil { log.Fatal("Failed to create tables:", err)
+		return
+	}
+	utils.StartSessionCleaner()
+}
+
+func main() {
+	m := http.NewServeMux()
+	server := &http.Server{
+		Addr:    ":8081",
+		Handler: l.RateMiddleware( routes.Router(m), 20, 100*time.Millisecond, ),
+	}
+	l.rm_sleep_users()
+	server.ListenAndServe()
+	fmt.Println("Database setup completed successfully!")
+}
