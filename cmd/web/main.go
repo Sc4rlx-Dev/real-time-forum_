eed3e261ec914b8fa17e36c72ab400806474410c
oerraoui <itsscarl2022@gmail.com>
2025-10-11 14:02:15 +0100

    refactor main server setup

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,34 +4,24 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"time"
-	// dbinit "realtimeforum/internal/database/db-init"
-	// "realtimeforum/internal/models"
-	// "realtimeforum/internal/routes"
-	// "realtimeforum/internal/utils"
 )
-var l = utils.NewRateLimiter()
-
-func init() {
-	var err error
-	models.Db, err = dbinit.OpenDatabase()
-	if err != nil { log.Fatal("Failed to open database:", err)
-		return
-	}
-	err = dbinit.Createtablue(models.Db)
-	if err != nil { log.Fatal("Failed to create tables:", err)
-		return
-	}
-	utils.StartSessionCleaner()
-}
 
 func main() {
-	m := http.NewServeMux()
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Server is running!")
+	})
 	server := &http.Server{
 		Addr:    ":8081",
-		Handler: l.RateMiddleware( routes.Router(m), 20, 100*time.Millisecond, ),
+		Handler: mux,
 	}
-	l.rm_sleep_users()
-	server.ListenAndServe()
-	fmt.Println("Database setup completed successfully!")
+
+	fmt.Println("Server is starting on http://localhost:8081")
+
+	// Start the server and log any errors that occur.
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("Server failed to start:", err)
+	}
 }
