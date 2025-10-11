6a03ca98b5d9f043b59053538e2957ceb91f779c
oerraoui <itsscarl2022@gmail.com>
2025-10-12 00:42:30 +0100

    create auth handlers | login() register()

---
+++ internal/handler/auth_handler.go
@@ -0,0 +1,61 @@
+package handler
+
+import (
+    "database/sql"
+    "encoding/json"
+    "net/http"
+    "real_time_forum/internal/models"
+    "real_time_forum/internal/repository"
+)
+
+
+
+
+type auth_handler struct {
+	DB *sql.DB
+}
+
+func (h *auth_handler) Register(w http.ResponseWriter, r *http.Request){
+	var user_data = models.user_data{
+		Username: "test",
+		FirstName: "Test",
+        LastName:  "User",
+        Age:       23,
+        Email:     "test@example.com",
+        Password:  "password123",
+        Gender:    "Male",
+	}
+	err := repository.Insert_user(h.DB , &user_data)
+	if err!=nil{
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(map[string]string{"message": "user registered successfully"})
+}
+
+
+func (h *AuthHandler) Login(w http.ResponseWriter, r *http.Request) {
+    var loginData models.Data
+    loginData = models.Data{
+        Username: "testuser",
+        Password: "password123",
+    }
+
+    userID, err := repository.AuthenticateUser(h.DB, &loginData)
+    if err != nil {http.Error(w, "Invalid credentials", http.StatusUnauthorized)
+        return
+    }
+
+    sessionToken, err := repository.CreateSession(h.DB, userID, loginData.Username)
+    if err != nil {http.Error(w, "Failed to create session", http.StatusInternalServerError)
+        return
+    }
+
+    http.SetCookie(w, &http.Cookie{
+        Name:     "session_token",
+        Value:    sessionToken,
+        HttpOnly: true,
+    })
+    json.NewEncoder(w).Encode(map[string]string{"message": "Logged in successfully"})
+}
