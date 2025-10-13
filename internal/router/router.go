14537f8296cea19e24b3d3c3446acfe1eca3b487
oerraoui <itsscarl2022@gmail.com>
2025-10-13 19:10:47 +0100

    refactor: update API route paths to include '/api' prefix for consistency

--- internal/router/router.go
+++ internal/router/router.go
@@ -12,11 +12,20 @@ func New_router(db *sql.DB) http.Handler {
     auth_h := &handler.Auth_handler{DB: db}
     post_h := &handler.Post_handler{DB: db}
 
-    m.HandleFunc("/register", auth_h.Register)
-    m.HandleFunc("/login", auth_h.Login)
-    m.HandleFunc("/posts/create", post_h.Auth_middleware(post_h.Create_post))
-    m.HandleFunc("/comments/add", post_h.Auth_middleware(post_h.Create_comment))
-    m.HandleFunc("/posts", post_h.Get_posts)
+    fs := http.FileServer(http.Dir("./web/static"))
+    m.Handle("/static/", http.StripPrefix("/static/" , fs))
 
-    return m
+    m.HandleFunc("/api/register", auth_h.Register)
+    m.HandleFunc("/api/login", auth_h.Login)  
+    m.HandleFunc("/api/posts/create", post_h.Auth_middleware(post_h.Create_post))
+    m.HandleFunc("/api/comments/add", post_h.Auth_middleware(post_h.Create_comment))
+    m.HandleFunc("/api/posts", post_h.Get_posts)
+
+    m.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path != "/" {
+            http.NotFound(w, r)
+            return}
+        http.ServeFile(w, r, "./web/templates/index.html")
+    })
+return m
 }
