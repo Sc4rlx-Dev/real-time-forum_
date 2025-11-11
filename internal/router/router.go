43efeaf937ab663ef5ac476c290c503aacb4cf25
oerraoui <itsscarl2022@gmail.com>
2025-11-11 18:43:20 +0100

    Refactor router.go for improved code formatting and consistency

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,42 +1,39 @@
 package router
 
 import (
-    "database/sql"
-    "net/http"
-    "real_time_forum/internal/handler"
+	"database/sql"
+	"net/http"
+	"real_time_forum/internal/handler"
 )
 
 func New_Router(db *sql.DB) http.Handler {
-    m := http.NewServeMux()
+	m := http.NewServeMux()
 
-    auth_h := &handler.Auth_handler{DB: db}
-    post_h := &handler.Post_handler{DB: db}
-    chat_h := &handler.Chat_handler{DB: db} // NEW
+	auth_h := &handler.Auth_handler{DB: db}
+	post_h := &handler.Post_handler{DB: db}
+	chat_h := &handler.Chat_handler{DB: db}
 
-    file_server := http.FileServer(http.Dir("./web/static"))
-    m.Handle("/static/", http.StripPrefix("/static/", file_server))
+	file_server := http.FileServer(http.Dir("./web/static"))
+	m.Handle("/static/", http.StripPrefix("/static/", file_server))
 
-    // API Routes
-    m.HandleFunc("/api/register", auth_h.Register)
-    m.HandleFunc("/api/login", auth_h.Login)
-    m.HandleFunc("/api/posts", post_h.Get_posts)
-    
-    // Authenticated API Routes
-    m.HandleFunc("/api/posts/create", post_h.Auth_middleware(post_h.Create_post))
-    m.HandleFunc("/api/comments/add", post_h.Auth_middleware(post_h.Create_comment))
-    m.HandleFunc("/api/users", post_h.Auth_middleware(chat_h.Get_users)) // NEW
-    m.HandleFunc("/api/messages/", post_h.Auth_middleware(chat_h.Get_messages)) // NEW
-    
-    // WebSocket Route
-    m.HandleFunc("/ws", handler.Ws_handler(db))
+	m.HandleFunc("/api/register", auth_h.Register)
+	m.HandleFunc("/api/login", auth_h.Login)
+	m.HandleFunc("/api/posts", post_h.Get_posts)
 
-    m.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        if r.URL.Path != "/" {
-            http.NotFound(w, r)
-            return
-        }
-        http.ServeFile(w, r, "./web/templates/index.html")
-    })
+	m.HandleFunc("/api/posts/create", post_h.Auth_middleware(post_h.Create_post))
+	m.HandleFunc("/api/comments/add", post_h.Auth_middleware(post_h.Create_comment))
+	m.HandleFunc("/api/users", post_h.Auth_middleware(chat_h.Get_users))
+	m.HandleFunc("/api/messages/", post_h.Auth_middleware(chat_h.Get_messages))
 
-    return m
+	m.HandleFunc("/ws", handler.Ws_handler(db))
+
+	m.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			http.NotFound(w, r)
+			return
+		}
+		http.ServeFile(w, r, "./web/templates/index.html")
+	})
+
+	return m
 }
